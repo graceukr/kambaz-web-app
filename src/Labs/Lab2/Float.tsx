926b2b94e22c72f883a23d756a7cbc6b2d86fac4
graceukr <graceukr@icloud.com>
2025-01-29 15:07:13 -0500

    2.1 styling react components with CSS

---
+++ src/Labs/Lab2/Float.tsx
@@ -0,0 +1,70 @@
+export default function Float() {
+    return (
+        <div>
+            <div id="wd-float-divs">
+                <h2>Float</h2>
+                <div>
+                <img className="wd-float-right"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sodales, velit eget ultrices porta, 
+                velit nisl luctus erat, sit amet scelerisque turpis risus at velit. Proin eleifend suscipit porttitor. 
+                Aenean quis erat nec nulla consequat congue. Praesent dignissim orci in arcu porta vulputate. Curabitur 
+                sodales quam quis maximus interdum. Duis fringilla mauris sed libero dignissim hendrerit. Pellentesque 
+                porttitor consectetur lacinia. Curabitur fermentum risus ligula, sit amet condimentum augue egestas ut.
+                Curabitur eu elementum ipsum. Donec blandit mi arcu, ut elementum neque condimentum a. Quisque eget 
+                aliquet nibh. Duis eleifend nulla quam, ac imperdiet nisi tempus id. Vestibulum consectetur quis nisi 
+                in sodales. Donec ipsum libero, cursus vitae ex sit amet, egestas tincidunt augue. Duis laoreet ullamcorper 
+                felis eu sagittis. Pellentesque ut varius justo, nec pretium arcu.
+                <img className="wd-float-left"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sodales, velit eget ultrices porta, 
+                velit nisl luctus erat, sit amet scelerisque turpis risus at velit. Proin eleifend suscipit porttitor. 
+                Aenean quis erat nec nulla consequat congue. Praesent dignissim orci in arcu porta vulputate. Curabitur 
+                sodales quam quis maximus interdum. Duis fringilla mauris sed libero dignissim hendrerit. Pellentesque 
+                porttitor consectetur lacinia. Curabitur fermentum risus ligula, sit amet condimentum augue egestas ut.
+                Curabitur eu elementum ipsum. Donec blandit mi arcu, ut elementum neque condimentum a. Quisque eget 
+                aliquet nibh. Duis eleifend nulla quam, ac imperdiet nisi tempus id. Vestibulum consectetur quis nisi 
+                in sodales. Donec ipsum libero, cursus vitae ex sit amet, egestas tincidunt augue. Duis laoreet ullamcorper 
+                felis eu sagittis. Pellentesque ut varius justo, nec pretium arcu.
+                <img className="wd-float-right"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sodales, velit eget ultrices porta, 
+                velit nisl luctus erat, sit amet scelerisque turpis risus at velit. Proin eleifend suscipit porttitor. 
+                Aenean quis erat nec nulla consequat congue. Praesent dignissim orci in arcu porta vulputate. Curabitur 
+                sodales quam quis maximus interdum. Duis fringilla mauris sed libero dignissim hendrerit. Pellentesque 
+                porttitor consectetur lacinia. Curabitur fermentum risus ligula, sit amet condimentum augue egestas ut.
+                Curabitur eu elementum ipsum. Donec blandit mi arcu, ut elementum neque condimentum a. Quisque eget 
+                aliquet nibh. Duis eleifend nulla quam, ac imperdiet nisi tempus id. Vestibulum consectetur quis nisi 
+                in sodales. Donec ipsum libero, cursus vitae ex sit amet, egestas tincidunt augue. Duis laoreet ullamcorper 
+                felis eu sagittis. Pellentesque ut varius justo, nec pretium arcu.
+                <img className="wd-float-left"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sodales, velit eget ultrices porta, 
+                velit nisl luctus erat, sit amet scelerisque turpis risus at velit. Proin eleifend suscipit porttitor. 
+                Aenean quis erat nec nulla consequat congue. Praesent dignissim orci in arcu porta vulputate. Curabitur 
+                sodales quam quis maximus interdum. Duis fringilla mauris sed libero dignissim hendrerit. Pellentesque 
+                porttitor consectetur lacinia. Curabitur fermentum risus ligula, sit amet condimentum augue egestas ut.
+                Curabitur eu elementum ipsum. Donec blandit mi arcu, ut elementum neque condimentum a. Quisque eget 
+                aliquet nibh. Duis eleifend nulla quam, ac imperdiet nisi tempus id. Vestibulum consectetur quis nisi 
+                in sodales. Donec ipsum libero, cursus vitae ex sit amet, egestas tincidunt augue. Duis laoreet ullamcorper 
+                felis eu sagittis. Pellentesque ut varius justo, nec pretium arcu.
+                <div className="wd-float-done"></div>
+                </div>
+            </div>
+            <div id="wd-float-divs">
+                <h2>Float</h2>
+                <div>
+                    <div className="wd-float-left wd-dimension-portrait wd-bg-color-yellow">
+                        Yellow </div>
+                    <div className="wd-float-left wd-dimension-portrait wd-bg-color-blue wd-fg-color-white">
+                        Blue </div>
+                    <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
+                        Red </div>
+                    <img className ="wd-float-right" 
+                        src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+                    <div className="wd-float-done"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
