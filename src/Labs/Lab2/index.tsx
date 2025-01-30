04403245a322232e11617a0cc3ca2eacee62f7d2
graceukr <graceukr@icloud.com>
2025-01-30 15:33:37 -0500

    2.3 styling webpages with the react bootstrap CSS library

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,4 +1,6 @@
 import "./index.css"
+import { Container } from "react-bootstrap"
+
 import ForegroundColors from "./ForegroundColors"
 import BackgroundColors from "./BackgroundColors"
 import Borders from "./Borders"
@@ -11,10 +13,17 @@ import Zindex from "./Zindex"
 import Float from "./Float"
 import GridLayout from "./GridLayout"
 import Flex from "./Flex"
+import ReactIconsSampler from "./ReactIcons"
+import BootstrapGrids from "./BootstrapGrids"
+import ScreenSizeLabel from "./ScreenSizeLabel"
+import BootstrapTables from "./BootstrapTables"
+import BootstrapLists from "./BootstrapLists"
+import BootstrapForms from "./BootstrapForms"
+import BootstrapNavigation from "./BootstrapNavigation"
 
 export default function Lab2() {
     return (
-        <div>
+        <Container>
             <h2>Lab 2 - Cascading Style Sheets</h2>
             <h3>Styling with the STYLE attribute</h3>
             <p>
@@ -77,6 +86,13 @@ export default function Lab2() {
             <Float />
             <GridLayout />
             <Flex />
-        </div>
+            <ReactIconsSampler />
+            <BootstrapGrids />
+            <ScreenSizeLabel />
+            <BootstrapTables />
+            <BootstrapLists />
+            <BootstrapForms />
+            <BootstrapNavigation />
+        </Container>
     )
 }
